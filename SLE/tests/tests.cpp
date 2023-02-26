7e82ff42fe5e5e2a07b4c0b29f126903409debbf
DinisKhafizov <91131119+DinisKhafizov@users.noreply.github.com>
2023-02-26 13:49:00 +0300

    Update tests.cpp

--- SLE/tests/tests.cpp
+++ SLE/tests/tests.cpp
@@ -2,51 +2,6 @@
 #include "Tridiag/Tridiag.hpp"
 #include "SolverTridiag/Solver.hpp"
 
-/*
-TEST(matrix, file1) {
-    std::string path1 = "SLE/tests/file1.txt";
-    std::vector<double> a = {1., 1.};
-    std::vector<double> b = {4., 4., 4.};
-    std::vector<double> c = {1., 1.};
-    TridiagonalMatrix A(path1);
-    for (int i = 0; i < A.GetN() - 2 ;++i) {
-        ASSERT_DOUBLE_EQ(a[i], A.GetA(i));
-        ASSERT_DOUBLE_EQ(b[i], A.GetB(i));
-        ASSERT_DOUBLE_EQ(c[i], A.GetC(i));
-    }
-    ASSERT_DOUBLE_EQ(b[A.GetN() - 1], A.GetB(A.GetN() - 1));
-
-}
-TEST(matrix, file2) {
-    std::string path2 = "SLE/tests/file2.txt";
-    std::vector<double> a = {5., 1.};
-    std::vector<double> b = {2., 4., -3.};
-    std::vector<double> c = {-1., 2.};
-    TridiagonalMatrix A(path2);
-    for (int i = 0; i < A.GetN() - 2 ;++i) {
-        ASSERT_DOUBLE_EQ(a[i], A.GetA(i));
-        ASSERT_DOUBLE_EQ(b[i], A.GetB(i));
-        ASSERT_DOUBLE_EQ(c[i], A.GetC(i));
-    }
-    ASSERT_DOUBLE_EQ(b[A.GetN() - 1], A.GetB(A.GetN() - 1));
-
-
-}
-TEST(matrix, file3) {
-    std::string path3 = "SLE/tests/file3.txt";
-    std::vector<double> a = {1., 3., 6., 2., 3., 11., 10., 1., 1.};
-    std::vector<double> b = {3.3, 5.2, 6.37, 12.03, 3., 5.6, 95., 102., 2., 1.};
-    std::vector<double> c = {2., 1., 2., 5., 1., 1., 50., 30., 0.5};
-    TridiagonalMatrix A(path3);
-    for (int i = 0; i < A.GetN() - 2 ;++i) {
-        ASSERT_DOUBLE_EQ(a[i], A.GetA(i));
-        ASSERT_DOUBLE_EQ(b[i], A.GetB(i));
-        ASSERT_DOUBLE_EQ(c[i], A.GetC(i));
-    }
-    ASSERT_DOUBLE_EQ(b[A.GetN() - 1], A.GetB(A.GetN() - 1));
-}
-
-*/
 TEST(solver, slae1) {
     std::vector<double> a = {1., 1.};
     std::vector<double> b = {4., 4., 4.};
